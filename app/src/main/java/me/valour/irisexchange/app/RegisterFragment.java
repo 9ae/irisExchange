8210ceee9485ea0a15cf9a2ad90352ddf5456df0
Alice Q. Wong <alice@bytemark.co>
2014-07-23 17:59:15 -0400

    register screen done

--- app/src/main/java/me/valour/irisexchange/app/RegisterFragment.java
+++ app/src/main/java/me/valour/irisexchange/app/RegisterFragment.java
@@ -5,10 +5,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
@@ -17,7 +20,11 @@ import com.koushikdutta.ion.Ion;
 
 public class RegisterFragment extends Fragment {
 
-    public RegisterListener regListener;
+    private RegisterListener regListener;
+    private EditText usernameField;
+    private EditText password0Field;
+    private EditText password1Field;
+    private EditText emailField;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -29,8 +36,15 @@ public class RegisterFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        usernameField = (EditText) view.findViewById(R.id.reg_username);
+        password0Field = (EditText) view.findViewById(R.id.reg_password0);
+        password1Field = (EditText) view.findViewById(R.id.reg_password1);
+        emailField = (EditText) view.findViewById(R.id.reg_email);
+
         Button button = (Button) view.findViewById(R.id.reg_go);
         button.setOnClickListener(regListener);
+
         return view;
     }
 
@@ -45,14 +59,47 @@ public class RegisterFragment extends Fragment {
         super.onDetach();
     }
 
+    protected void gotToken(String token){
+        Log.d("val","token = "+token);
+    }
 
-    public class RegisterListener implements View.OnClickListener{
+
+    protected class RegisterListener implements View.OnClickListener{
         @Override
         public void onClick(View view) {
+
+            String password0 = password0Field.getText().toString();
+            String password1 = password1Field.getText().toString();
+            if(!password0.equals(password1)){
+                Toast toast = Toast.makeText(view.getContext(), getString(R.string.toast_passwords_dont_match), Toast.LENGTH_SHORT);
+                toast.show();
+                return;
+            }
+            String username = usernameField.getText().toString();
+            String email = emailField.getText().toString();
+
+            if(username.isEmpty()){
+                Toast toast = Toast.makeText(view.getContext(), getString(R.string.toast_not_empty,"Username"), Toast.LENGTH_SHORT);
+                toast.show();
+                return;
+            }
+
+            if(email.isEmpty()){
+                Toast toast = Toast.makeText(view.getContext(), getString(R.string.toast_not_empty,"E-mail address"), Toast.LENGTH_SHORT);
+                toast.show();
+                return;
+            }
+
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                Toast toast = Toast.makeText(view.getContext(), getString(R.string.toast_invalid_email), Toast.LENGTH_SHORT);
+                toast.show();
+                return;
+            }
+
             JsonObject requestBody = new JsonObject();
-            requestBody.addProperty("email","s@b.com");
-            requestBody.addProperty("username","sunny");
-            requestBody.addProperty("password", "bite");
+            requestBody.addProperty("email",email);
+            requestBody.addProperty("username",username);
+            requestBody.addProperty("password", password0);
             String url = getString(R.string.base_url)+"/register";
             Log.d("point",url);
             Ion.with(view.getContext())
@@ -67,7 +114,7 @@ public class RegisterFragment extends Fragment {
                                 return;
                             }
                             String token = jsonObject.get("token").getAsString();
-                            Log.d("val","token="+token);
+                            gotToken(token);
                         }
                     });
         }
